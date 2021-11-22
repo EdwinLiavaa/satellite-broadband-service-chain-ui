0d483b039f04fd581d802f3eb7d55cd24c5c33b0
FidelChe <etuini.liavaa@gmail.com>
2021-11-22 20:55:28 +1100

    Chainlink Hackathon Fall 2021

--- app/static/js/logic.js
+++ app/static/js/logic.js
@@ -5,12 +5,6 @@ Moralis.serverURL = ""; //Server url from moralis.io
 const nft_contract_address = "" //NFT Minting Contract 
 const api_consumer_address = "" //Marine Traffic External Adapter Contract
 const service_chain_address = "" //Service Chain Contract
-/*
-Available deployed contracts
-Ethereum Rinkeby 0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431
-Polygon Mumbai 0x351bbee7C6E9268A1BF741B098448477E08A0a53
-BSC Testnet 0x88624DD1c725C6A95E223170fa99ddB22E1C6DDD
-*/
 
 const web3 = new Web3(window.ethereum);
 
